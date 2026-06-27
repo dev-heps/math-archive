625c2d789ec0713172c6b815c690cd0606042cfe
dev-heps <hepsdata@yonsei.ac.kr>
2026-06-27 19:35:32 +0900

    Initialize math-heps.github.io

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+import createMDX from '@next/mdx'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+};
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+})
+
+export default withMDX(nextConfig);
